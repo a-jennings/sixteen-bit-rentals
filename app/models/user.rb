e63e1cee23dc67b5a472b6a456074e9da9b7f109
Drew Jennings <andrew_jennings@ymail.com>
2021-08-23 16:18:19 +0100

    generate user model, and devise views

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :address, presence: true
+  validates :first_name, :last_name, format: { with: /^[a-zA-Z]+$/, message: "only allows letters" }
+end
