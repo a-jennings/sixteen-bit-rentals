9a3ae9de150dc3d0ff21d535a4c5ac4d1fdb0998
Xander Reardon <xanderreardon@gmail.com>
2021-08-24 12:36:58 +0100

    remove skip before action

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,5 +1,4 @@
 class ConsolesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
   before_action :console_list, only: [:show, :edit, :update]
 
   def index
