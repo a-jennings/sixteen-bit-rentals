1dece428f2b326133f6166be3a4858d84b6db0ed
Drew Jennings <andrew_jennings@ymail.com>
2021-08-24 17:38:37 +0100

    Update rental.rb
    remove console log for debug

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -6,7 +6,6 @@ class Rental < ApplicationRecord
   validate :date_logic
 
   def date_logic
-    p "In date logic"
     if Date.today > start_rental_date
       errors.add(:start_rental_date, "date cannot be in the past")
     elsif Date.today > end_rental_date
