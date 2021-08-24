a133f6824753a3cd1b15eff959de85c4f03fb2ed
Jacob Chew <jacobjcchew@gmail.com>
2021-08-24 11:01:02 +0100

    Updated users routes using resources, only method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/profile'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/destroy'
+  # get 'users/profile'
+  resources :users, only: %i[show new create destroy]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
