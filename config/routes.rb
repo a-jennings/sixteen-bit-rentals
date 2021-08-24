1408a84c7a2f1615f575d9b788106da92ddee5a7
Drew Jennings <andrew_jennings@ymail.com>
2021-08-24 14:51:16 +0100

    fix routes file, and authentication for console controller so that index page shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # get 'users/profile'
   # resources :users, only: %i[show new create destroy]
+  root to: 'pages#home'
+  devise_for :users
   get '/profile', to: 'users#profile'
   get '/users/:id', to: 'users#show'
-  devise_for :users
-  root to: 'pages#home'
   resources :consoles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
