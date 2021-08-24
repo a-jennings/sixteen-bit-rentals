f8ec0206765b8d4c1ec9022054f1d94d0967f810
Drew Jennings <andrew_jennings@ymail.com>
2021-08-24 11:17:45 +0100

    add forms to console new and edit path

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_150613) do
+ActiveRecord::Schema.define(version: 2021_08_24_094748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "consoles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.integer "price_per_day"
+    t.integer "min_rental_time"
+    t.integer "max_rental_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_consoles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_08_23_150613) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consoles", "users"
 end
