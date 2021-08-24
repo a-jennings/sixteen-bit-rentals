5282a4c47a14d5c4c2f64df7604c93a5acef4640
Louis Drinkwater <izumilouis95@gmail.com>
2021-08-24 16:10:35 +0100

    gemfile.lock stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :address, presence: true
   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  has_many :consoles
+  has_many :consoles, dependent: :destroy
 end
