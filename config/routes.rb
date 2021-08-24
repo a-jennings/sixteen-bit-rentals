9b57c34feb78753cd5423d6ca9ec3e3578c8db99
Jacob Chew <jacobjcchew@gmail.com>
2021-08-24 15:07:56 +0100

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
 
   # get 'users/profile'
   # resources :users, only: %i[show new create destroy]
+
+  root to: 'pages#home'
+  # root to: 'devise/registrations#new'
+  devise_for :users
+  # devise_scope :users do
+  #   root 'devise/registrations#new'
+  # end
   get '/profile', to: 'users#profile'
   get '/users/:id', to: 'users#show'
-
-  devise_for :users
-  root to: 'pages#home'
   resources :consoles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
