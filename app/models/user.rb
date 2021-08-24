ab01889e2bae32a830a40d500f551c421eac3b45
Louis Drinkwater <izumilouis95@gmail.com>
2021-08-24 17:55:43 +0100

    Add phone number regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :address, presence: true
-  validates :first_name, :last_name, format: { with: /\A([a-z]+[ \-.,']*)+\z/i, message: "only allows letters" }
+  validates :first_name, :last_name, format: { with: /\A([a-z]+[ \-.,']*)+\z/i,
+                                               message: "Please use only letters from the latin alphabet, spaces, and punctuation: -.,'" }
+  validates :phone_number, format: { with: /\A\(?\+?(\d+[ .\-\/()]*)+\z/,
+                                     message: "Please use only numbers 0-9 and punctuation: +.-/()" }
   has_many :consoles, dependent: :destroy
   has_many :rentals
 end
