122556ba038c2f8cfad6c6114007e27d3630ff2c
Jacob Chew <jacobjcchew@gmail.com>
2021-08-24 10:50:02 +0100

    Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
