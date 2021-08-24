d6d6af222420aadfd7669bb003ab09ec0a2aa80b
Jacob Chew <jacobjcchew@gmail.com>
2021-08-24 12:50:42 +0100

    Added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
   def profile
+    @user = current_user
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # get 'users/profile'
-  resources :users, only: %i[show new create destroy]
+  # resources :users, only: %i[show new create destroy]
+  get '/profile', to: 'users#profile'
+  get '/users/:id', to: 'users#show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
