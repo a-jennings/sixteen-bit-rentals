853b0197c3b5b88b7b65be02baedb8025d0f47b0
Drew Jennings <andrew_jennings@ymail.com>
2021-08-24 11:08:18 +0100

    additional validations for console model

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,6 +1,6 @@
 class Console < ApplicationRecord
   belongs_to :user
 
-  validates :user, presence: true
+  validates :user, :price_per_day, :min_rental_time, :max_rental_time, presence: true
   validates :name, length: { minimum: 6 }
 end
