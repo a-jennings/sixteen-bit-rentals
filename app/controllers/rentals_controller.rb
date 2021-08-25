9f69970ad5aba58fff5d052f78f24bfae5210319
Drew Jennings <andrew_jennings@ymail.com>
2021-08-25 11:50:57 +0100

    finish basic skeleteon for rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,5 @@
 class RentalsController < ApplicationController
+
   def new
     @console = Console.find(params[:console_id])
     @owner = User.find(@console.user_id)
@@ -11,8 +12,7 @@ class RentalsController < ApplicationController
     @rental = Rental.new(start_rental_date: Date.today,
                          end_rental_date: Date.today + (params[:rental][:current_rental_days]).to_i,
                          console_id: @console.id,
-                         user_id: @owner.id
-                        )
+                         user_id: @owner.id)
     if @rental.save!
       redirect_to console_path(@console)
     else
