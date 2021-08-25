41118e22232e6602234a763e3941f30f1e2d3ebd
Drew Jennings <andrew_jennings@ymail.com>
2021-08-25 10:56:36 +0100

    add more html fields, and @rental to controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,13 +2,16 @@ class RentalsController < ApplicationController
 
   def new
     @console = Console.find(params[:console_id])
+    @owner = User.find(@console.user_id)
+    @rental = Rental.new
+  end
+
+  def create
   end
 
   def edit
   end
 
-  def create
-  end
 
   def update
   end
