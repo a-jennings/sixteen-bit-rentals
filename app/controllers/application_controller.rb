06d04b21b91720ed035e6c029a4565ace1e497bb
Drew Jennings <andrew_jennings@ymail.com>
2021-08-25 15:14:48 +0100

    added redirect path after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :address])
   end
+
+  def after_sign_in_path_for(resource)
+    consoles_path
+  end
 end
