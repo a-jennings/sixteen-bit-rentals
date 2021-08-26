aed270e54487f4aaa541cdf4735ee904e81b3c93
Drew Jennings <andrew_jennings@ymail.com>
2021-08-26 14:34:48 +0100

    Complete all CRUD actions for rentals. Refactor rental controller code

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,14 +1,13 @@
 class RentalsController < ApplicationController
+  before_action :gets_console
+  before_action :gets_owner, only: %i[new create edit update]
+  before_action :gets_rental, only: %i[]
 
   def new
-    @console = Console.find(params[:console_id])
-    @owner = User.find(@console.user_id)
     @rental = Rental.new
   end
 
   def create
-    @console = Console.find(params[:console_id])
-    @owner = User.find(@console.user_id)
     @rental = Rental.new(start_rental_date: rental_params[:start_rental_date],
                          end_rental_date: rental_params[:end_rental_date],
                          console_id: @console.id,
@@ -21,28 +20,38 @@ class RentalsController < ApplicationController
     end
   end
 
-  def show
-    @console = Console.find(params[:console_id])
-    @rental = Rental.where(console_id: params[:console_id])
-  end
+  def show; end
 
-  def edit
-    @console = Console.find(params[:console_id])
-    @rental = Rental.where(console_id: params[:console_id])
-    @owner = User.find(@console.user_id)
-  end
+  def edit; end
 
   def update
-    @console = Console.find(params[:console_id])
-    @owner = User.find(@console.user_id)
     @rental.update(start_rental_date: rental_params[:start_rental_date],
                    end_rental_date: rental_params[:end_rental_date])
+    if @rental.save
+      redirect_to console_rental_path(@console.id, @rental)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @rental.delete
+    redirect_to console_path(@console)
   end
 
   private
+
+  def gets_console
+    @console = Console.find(params[:console_id])
+  end
+
+  def gets_owner
+    @owner = User.find(@console.user_id)
+  end
+
+  def gets_rental
+    @rental = Rental.where("console_id = ?", @console.id)[0]
+  end
 
   def rental_params
     params.require(:rental).permit(:current_rental_days, :start_rental_date, :end_rental_date)
